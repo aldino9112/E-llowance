79abba56770350a7b1d50fc4eb2695db3ef2a0f1
aldino9112 <aldino.dian@gmail.com>
2020-07-31 16:20:13 +0700

    done

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById("mainButton").onclick = function () {
+    var ballanceAwal = 0
+    ballanceAwal += document.getElementById('baseBallance').value;
+    location.href = "main.html"
+}
